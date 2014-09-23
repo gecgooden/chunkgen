772d2113ffdfe4530de8119c0826bf0674ab4519
George Gooden <gecgooden@gmail.com>
2014-09-23 20:23:37 +0800

    Initial Commit

---
+++ src/main/java/com/gecgooden/chunkgen/ChunkGen.java
@@ -0,0 +1,33 @@
+package com.gecgooden.chunkgen;
+
+import java.util.Map;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import cpw.mods.fml.common.network.NetworkCheckHandler;
+import cpw.mods.fml.relauncher.Side;
+
+@Mod(modid = ChunkGen.MODID, version = ChunkGen.VERSION)
+public class ChunkGen
+{
+    public static final String MODID = "chunkgen";
+    public static final String VERSION = "1.7.10-1.0";
+    
+    /**
+     * Makes the mod server side only
+     * @param map
+     * @param side
+     * @return Always true.
+     */
+    @NetworkCheckHandler
+    public boolean networkCheckHandler(Map<String, String> map, Side side) { 
+    	return true;
+    }
+    
+    @EventHandler
+    public void serverLoad(FMLServerStartingEvent event)
+    {
+      event.registerServerCommand(new ChunkGenCommand());
+    }
+}
---
+++ src/main/java/com/gecgooden/chunkgen/ChunkGenCommand.java
@@ -0,0 +1,150 @@
+package com.gecgooden.chunkgen;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.ChunkProviderServer;
+
+public class ChunkGenCommand implements ICommand
+{	
+	private List aliases;
+	public ChunkGenCommand()
+	{
+		this.aliases = new ArrayList();
+		this.aliases.add("chunkgen");
+	}
+
+	@Override
+	public String getCommandName()
+	{
+		return "chunkgen";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender icommandsender)
+	{
+		return "chunkgen <x> <y> <height> <width> [dimension]";
+	}
+
+	@Override
+	public List getCommandAliases()
+	{
+		return this.aliases;
+	}
+
+	/**
+	 * Return the required permission level for this command.
+	 */
+	public int getRequiredPermissionLevel()
+	{
+		return 4;
+	}
+
+	@Override
+	public void processCommand(ICommandSender icommandsender, String[] astring)
+	{
+		if(!icommandsender.canCommandSenderUseCommand(getRequiredPermissionLevel(), this.getCommandName())) {
+			ChatComponentTranslation chatTranslation = new ChatComponentTranslation("commands.generic.permission", new Object[0]);
+			MinecraftServer.getServer().addChatMessage(chatTranslation);
+			icommandsender.addChatMessage(new ChatComponentText(chatTranslation.getUnformattedTextForChat()));
+		} else {
+			int playerX = 0;
+			int playerY = 0;
+			int playerZ = 0;
+			if(!icommandsender.getCommandSenderName().equalsIgnoreCase("Rcon")) {
+				EntityPlayer ep = MinecraftServer.getServer().worldServerForDimension(0).getPlayerEntityByName(icommandsender.getCommandSenderName());
+				ChunkCoordinates cc = icommandsender.getPlayerCoordinates();
+				playerX = cc.posX;
+				playerY = cc.posY;
+				playerZ = cc.posZ;
+			}
+			try {
+				int x = 0;
+				int z = 0;
+				if(astring[0].equalsIgnoreCase("~")) {
+					x = playerX/16;
+				} else {
+					x = Integer.parseInt(astring[0]);
+				}
+				if(astring[1].equalsIgnoreCase("~")) {
+					z = playerZ/16;
+				} else {
+					z = Integer.parseInt(astring[1]);
+				}
+				int height = Integer.parseInt(astring[2]);
+				int width = Integer.parseInt(astring[3]);
+				int dimensionID = 0;
+				if(astring.length == 5) {
+					dimensionID = Integer.parseInt(astring[4]);
+				}
+
+				System.out.println(x + " " + z + " " + height + " " + width + " " + dimensionID);
+
+				ChunkProviderServer cps = MinecraftServer.getServer().worldServerForDimension(dimensionID).theChunkProviderServer;
+				List<Chunk> chunks = new ArrayList<Chunk>(width*height);
+				for(int i = (x - width/2); i < (x + width/2); i++) {
+					for(int j = (z - height/2); j < (z + height/2); j++) {
+						System.out.println("About to generate chunk at " + i + " " + j);
+						if(!cps.chunkExists(i, j)) {
+							chunks.add(cps.loadChunk(i, j)); 
+							cps.saveChunks(true, null);
+						}
+						System.out.println("Loaded Chunk at " + i + " " + j);
+					}
+				}
+				for(Chunk c : chunks) {
+					cps.unloadChunksIfNotNearSpawn(c.xPosition, c.zPosition);
+				}
+				ChatComponentTranslation chatTranslation = new ChatComponentTranslation("commands.successful");
+				MinecraftServer.getServer().addChatMessage(chatTranslation);
+				icommandsender.addChatMessage(new ChatComponentText(chatTranslation.getUnformattedTextForChat()));
+			} catch (NumberFormatException e) {
+				e.printStackTrace();
+				ChatComponentTranslation chatTranslation = new ChatComponentTranslation("commands.numberFormatException");
+				MinecraftServer.getServer().addChatMessage(chatTranslation);
+				icommandsender.addChatMessage(new ChatComponentText(chatTranslation.getUnformattedTextForChat()));
+			} catch (Exception e) {
+				e.printStackTrace();
+				ChatComponentTranslation chatTranslation = new ChatComponentTranslation("commands.failed");
+				MinecraftServer.getServer().addChatMessage(chatTranslation);
+				icommandsender.addChatMessage(new ChatComponentText(chatTranslation.getUnformattedTextForChat()));
+			}
+			
+		}
+	}
+
+	@Override
+	public boolean canCommandSenderUseCommand(ICommandSender icommandsender)
+	{	
+		return true;
+	}
+
+	@Override
+	public List addTabCompletionOptions(ICommandSender icommandsender,
+			String[] astring)
+	{
+		return null;
+	}
+
+	@Override
+	public boolean isUsernameIndex(String[] astring, int i)
+	{
+		return false;
+	}
+
+	@Override
+	public int compareTo(Object o)
+	{
+		return 0;
+	}
+}

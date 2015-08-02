307a72578c174c59683c5a170038ebd8ddbf4c39
George Gooden <gecgooden@gmail.com>
2015-08-02 18:13:29 +0800

    Simplified the method names in reference to #6 and removed old comments

--- src/main/java/com/gecgooden/chunkgen/util/Utilities.java
+++ src/main/java/com/gecgooden/chunkgen/util/Utilities.java
@@ -27,7 +27,7 @@ public class Utilities {
 		List<Chunk> chunks = new ArrayList<Chunk>(width*height);
 		for(int i = (x - width/2); i < (x + width/2); i++) {
 			for(int j = (z - height/2); j < (z + height/2); j++) {
-				generateChunk(null, i, j, dimensionID);
+				generateChunk(i, j, dimensionID);
 			}
 		}
 		for(Chunk c : chunks) {
@@ -35,7 +35,7 @@ public class Utilities {
 		}
 	}
 
-	private static boolean chunksExist(ChunkProviderServer cps, int x, int z, int dimensionID) {
+	private static boolean chunksExist(int x, int z, int dimensionID) {
 		WorldServer world = null;
 	
 		world = DimensionManager.getWorld(dimensionID);
@@ -45,25 +45,15 @@ public class Utilities {
 		
 	}
 	
-	public static void generateChunk(ChunkProviderServer cps, int x, int z, int dimensionID) {
-		if(cps == null) {
-			cps = MinecraftServer.getServer().worldServerForDimension(dimensionID).theChunkProviderServer;
-		}
-		if(!chunksExist(cps, x, z, dimensionID)) {
+	public static void generateChunk(int x, int z, int dimensionID) {
+		ChunkProviderServer cps = MinecraftServer.getServer().worldServerForDimension(dimensionID).theChunkProviderServer;
+		if(!chunksExist(x, z, dimensionID)) {
 			cps.loadChunk(x, z);
 
 			cps.loadChunk(x, z+1);
 			cps.loadChunk(x+1, z);
 			cps.loadChunk(x+1, z+1);
 			
-//			cps.unloadChunksIfNotNearSpawn(x, z);
-//			
-//			cps.unloadChunksIfNotNearSpawn(x, z+1);
-//			cps.unloadChunksIfNotNearSpawn(x, z-1);
-//			cps.unloadChunksIfNotNearSpawn(x+1, z);
-//			cps.unloadChunksIfNotNearSpawn(x-1, z);
-			
-
 			Reference.logger.info("Loaded Chunk at " + x + " " + z + " " + dimensionID);
 		}
 	}

137ae087978c5a77c70c0030759ec510bf8857ec
Sythelux Rikd <dersyth@gmail.com>
2017-04-15 16:19:39 +0200

    * simplified everything and it's working pretty fine

--- src/main/java/com/gecgooden/chunkgen/ChunkGen.java
+++ src/main/java/com/gecgooden/chunkgen/ChunkGen.java
@@ -24,18 +24,18 @@ public class ChunkGen
 
 		Reference.decimalFormat = new DecimalFormat();
 		Reference.decimalFormat.setMaximumFractionDigits(2);
-		
+
 		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
 		MinecraftForge.EVENT_BUS.register(new ConfigurationHandler());
 		MinecraftForge.EVENT_BUS.register(new TickHandler());
 	}
 
 	@EventHandler
-	public void serverLoad(FMLServerStartingEvent event)
-	{
-		event.registerServerCommand(new ChunkGenCommand());
-		if(Reference.x != null && Reference.z != null && Reference.height != null && Reference.width != null && Reference.height > 0 && Reference.width > 0) {
-			Utilities.queueChunkGeneration(event.getServer(), Reference.skipChunks, Reference.x, Reference.z, Reference.height, Reference.width, 0, false);
-		}
-	}
+    public void serverLoad(FMLServerStartingEvent event) {
+        event.registerServerCommand(new ChunkGenCommand());
+        if (Reference.x != null && Reference.z != null && Reference.depth != null && Reference.width != null && Reference.depth > 0 && Reference.width > 0) {
+            Reference.logger.info(String.format("Starting initial Generation with x:%d z:%d width:%d depth:%d", Reference.x, Reference.z, Reference.width, Reference.depth));
+            Utilities.queueChunkGeneration(event.getServer(), Reference.skipChunks, Reference.x, Reference.z, Reference.width, Reference.depth, 0, false);
+        }
+    }
 }
--- src/main/java/com/gecgooden/chunkgen/reference/Reference.java
+++ src/main/java/com/gecgooden/chunkgen/reference/Reference.java
@@ -5,12 +5,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.text.DecimalFormat;
 import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 
 public class Reference {
     public static Integer x;
     public static Integer z;
-    public static Integer height;
+    public static Integer depth;
     public static Integer width;
     public static double numChunksPerTick;
     public static boolean pauseForPlayers;
@@ -20,7 +20,7 @@ public class Reference {
     public static final String VERSION = "1.3";
     public static final String GUI_FACTORY = "com.gecgooden.chunkgen.client.gui.GuiFactory";
 
-    public static Queue<ChunkPosition> toGenerate = new LinkedList<ChunkPosition>();
+    public static LinkedList<ChunkPosition> toGenerate = new LinkedList<>();
     public static int startingSize;
     public static int updateDelay;
 
